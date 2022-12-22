bf160c3813f72ce60b7d2054e8503518ff2069e8
Yuxin Ye <yuxin_ye@protonmail.com>
2022-12-22 16:44:35 +1100

    configure reducer for the app

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,9 +4,18 @@ import App from "./App";
 import "./index.css";
 import { configureStore } from "@reduxjs/toolkit";
 import globalReducer from "./state";
+import { Provider } from "react-redux";
+
+const store = configureStore({
+  reducer: {
+    global: globalReducer,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
