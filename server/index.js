c5b25c5653642ba7b3de4cd627c1ea9349d46d06
Yuxin Ye <yuxin_ye@protonmail.com>
2023-01-25 16:59:20 +1100

    add one time code to populate mongo db

--- server/index.js
+++ server/index.js
@@ -10,6 +10,10 @@ import generalRoutes from "./routes/general.js";
 import managementRoutes from "./routes/management.js";
 import salesRoutes from "./routes/sales.js";
 
+// data imports
+import User from "./models/User.js";
+import { dataUser } from "./data/index.js";
+
 // Configurations
 dotenv.config();
 const app = express();
@@ -37,5 +41,8 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+    // Only add data to the database if it's empty
+    // User.insertMany(dataUser);
   })
   .catch((error) => console.log(`${error} didn't connect. `));
