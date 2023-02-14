ae8f9c977844bcc53ac90fe59e07110a1c4c7836
Yuxin Ye <yuxin_ye@protonmail.com>
2023-02-14 15:21:54 +1100

    add products components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import { useSelector } from "react-redux";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./scenes/Layout";
 import Dashboard from "./scenes/dashboard";
+import Products from "./scenes/products";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -20,6 +21,7 @@ function App() {
               {/* direct to dashboard when user login  */}
               <Route path="/" element={<Navigate to="/dashboard" replace />} />
               <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/products" element={<Products />} />
             </Route>
           </Routes>
         </ThemeProvider>
