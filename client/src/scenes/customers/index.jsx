edf43cd45796b8abcc8b5daba84c46aea5aeec41
Yuxin Ye <yuxin_ye@protonmail.com>
2023-03-02 16:43:11 +1100

    add in customer components

---
+++ client/src/scenes/customers/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetCustomersQuery } from "../../state/api";
+import Header from "../../components/Header";
+import { DataGrid } from "@mui/x-data-grid";
+
+const Customers = () => {
+  const theme = useTheme();
+  const { data, isLoading } = useGetCustomersQuery();
+  return <div>Customers</div>;
+};
+
+export default Customers;
