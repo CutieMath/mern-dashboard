8073dadad8ee25604f6b74aee07b2cbaeae39fed
Yuxin Ye <yuxin_ye@protonmail.com>
2023-03-08 15:45:13 +1100

    add in content for geography chart

--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Box, useTheme } from "@mui/material";
+import { useGetGeographyQuery } from "../../state/api";
+import Header from "../../components/Header";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoData } from "../../state/geoData";
 
 const Geography = () => {
-  return <div>Geography</div>;
+  const theme = useTheme();
+  const { data } = useGetGeographyQuery();
+  console.log("data", data);
+  return (
+    <Box m="1.5rem 2.5rem">
+      <Header title="GEOGRAPHY" subtitle="User location" />
+      <Box
+        mt="40px"
+        height="75vh"
+        border={`1px solid ${theme.palette.secondary[200]}`}
+        borderRadius="2px"
+      >
+        {data ? <></> : <>Loading...</>}
+      </Box>
+    </Box>
+  );
 };
 
 export default Geography;
