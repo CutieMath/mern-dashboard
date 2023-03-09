fb3cbfa46eb5006e99e00743a1e312302dc0a8f0
Yuxin Ye <yuxin_ye@protonmail.com>
2023-03-09 14:50:34 +1100

    add geo graph into the dashboard

--- client/src/scenes/geography/index.jsx
+++ client/src/scenes/geography/index.jsx
@@ -18,7 +18,50 @@ const Geography = () => {
         border={`1px solid ${theme.palette.secondary[200]}`}
         borderRadius="2px"
       >
-        {data ? <></> : <>Loading...</>}
+        {data ? (
+          <ResponsiveChoropleth
+            data={data}
+            features={geoData.features}
+            margin={{ top: 0, right: 0, bottom: 0, left: 0 }}
+            colors="nivo"
+            domain={[0, 1000000]}
+            unknownColor="#666666"
+            label="properties.name"
+            valueFormat=".2s"
+            projectionTranslation={[0.5, 0.5]}
+            projectionRotation={[0, 0, 0]}
+            graticuleLineColor="#dddddd"
+            borderWidth={0.5}
+            borderColor="#152538"
+            legends={[
+              {
+                anchor: "bottom-left",
+                direction: "column",
+                justify: true,
+                translateX: 20,
+                translateY: -100,
+                itemsSpacing: 0,
+                itemWidth: 94,
+                itemHeight: 18,
+                itemDirection: "left-to-right",
+                itemTextColor: "#444444",
+                itemOpacity: 0.85,
+                symbolSize: 18,
+                effects: [
+                  {
+                    on: "hover",
+                    style: {
+                      itemTextColor: "#000000",
+                      itemOpacity: 1,
+                    },
+                  },
+                ],
+              },
+            ]}
+          />
+        ) : (
+          <>Loading...</>
+        )}
       </Box>
     </Box>
   );
