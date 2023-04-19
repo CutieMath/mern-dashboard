3780a29a0a42e4fae0cf3ef44061f9534b0e5818
Yuxin Ye <yuxin_ye@protonmail.com>
2023-04-19 17:11:38 +1000

    add imports into dashboard

--- client/src/scenes/dashboard/index.jsx
+++ client/src/scenes/dashboard/index.jsx
@@ -1,4 +1,23 @@
 import React from "react";
+import FlexBetween from "../../components/FlexBetween";
+import Header from "../../components/Header";
+import {
+  DownloadOutlined,
+  Email,
+  PointOfSale,
+  PersonAdd,
+  Traffic,
+} from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
+import BreakdownChart from "../../components/BreakdownChart";
+import OverviewChart from "../../components/OverviewChart";
 
 const Dashboard = () => {
   return <div>Dashboard</div>;
